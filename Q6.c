2b1806466cc02bb50a777ed15e3cd2ca9b63e3f8
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-20 23:09:05 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,14 @@
+//Write a program to calculate factorial of a number
+#include"stdio.h"
+int main()
+{
+    int i,n,fact=1;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+fact=fact*i;
+    }
+    printf("factorial of %d is %d",n,fact);
+    printf("\n");
+    return 0;
+}
